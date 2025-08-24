b6d62cbbec7bf0c92aa4b968c5d3b10254f1c16d
Scarce Koi <scarcekoi@gmail.com>
2025-08-24 17:53:52 -0400

    feat: transform emojis low quality render

--- emojis.py
+++ emojis.py
@@ -4,7 +4,7 @@ from catppuccin import PALETTE
 
 mocha = PALETTE.mocha.colors
 
-class transform_emojis(MovingCameraScene):
+class TransformEmojis(MovingCameraScene):
     def construct(self):
         self.camera.background_color = mocha.crust.hex
 
@@ -12,13 +12,15 @@ class transform_emojis(MovingCameraScene):
         files = sorted(f for f in os.listdir(folder) if f.endswith(".svg"))
         emojis = [SVGMobject(os.path.join(folder, f)).scale(1.5) for f in files]
 
-        # Start with the first emoji in the center
+        y_center = 0
+        x_center = 0
+
         current = emojis[0]
+        current.move_to([x_center, y_center, 0])
         self.add(current)
         self.wait(0.5)
 
-        # Transition through the rest using Transform
         for next_emoji in emojis[1:]:
-            next_emoji.move_to(current.get_center())
+            next_emoji.move_to([x_center, y_center, 0])
             self.play(Transform(current, next_emoji), run_time=0.5)
             self.wait(0.1)
